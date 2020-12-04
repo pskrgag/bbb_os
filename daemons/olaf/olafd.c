84784bce9555fc73e073d9f5a1a0cfe7cd94d41e
Skripkin Pavel <paskripkin@gmail.com>
2020-12-04 21:00:43 +0300

    added .gitignore + older stuff added to commit

--- daemons/olaf/olafd.c
+++ daemons/olaf/olafd.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include <pthread.h>
 
-#include <bone/olaf.h>
+#include <bone/olaf_api.h>
 #include "olaf.h"
 
 static int olaf_login(sock_t socket)
@@ -15,6 +15,11 @@ static int olaf_login(sock_t socket)
 	uint8_t login_attemts = 0; /* only 3 login attemps allowed */
 	int res;
 	struct olaf_login_args args;
+	olaf_code_t code;
+
+	code = olaf_get_code(socket);
+	if (code != OLAF_LOGIN)
+		return -1;
 
 	res = recv(socket, &args, sizeof(args), 0);
 	if (res != sizeof(args)) {
@@ -33,19 +38,15 @@ static void *__attribute__((noreturn)) user_main(void *data)
 
 	/* login routine */
 	/* user must login first, if he can't login, so close the connection */
-	code = olaf_get_code(socket);
-	DBG("Got code %llu %llu!!!\n", code, OLAF_LOGIN);
-	if (code != OLAF_LOGIN)
-		goto error;
-	else
-		is_logged = olaf_login(socket);
-
+	is_logged = olaf_login(socket);
 	if (is_logged <= 0)
 		goto error;
 
 	DBG("Logged!!!\n");
 	while (1) {
 		code = olaf_get_code(socket);
+		if (code == OLAF_WRONG_CODE)		/* no need to talk with broken connection */
+			goto error;
 	}
 error:
 	close(socket);
@@ -57,6 +58,7 @@ static void __attribute__((noreturn)) olaf_main(sock_t master)
 	sock_t new_conn;
 	pthread_t user_thread;
 	struct user_args *args;
+	int res;
 
 	while (1) {
 		new_conn = accept(master, NULL, NULL);
@@ -65,9 +67,24 @@ static void __attribute__((noreturn)) olaf_main(sock_t master)
 			continue;
 		}
 
+		res = olaf_enable_keep_alive(new_conn);
+		if (res) {
+			log_err("Failed to set keep_alive for client");
+			goto err;
+		}
+
+		res = olaf_check_handshake(new_conn);
+		if (res) {
+			log_err("Failed to check handshake with user");
+			goto err;
+		}
+
 		pthread_create(&user_thread, NULL, user_main, (void *) new_conn);
 
 		DBG("Accepted new connection\n");
+		continue;
+err:
+		close(new_conn);
 	}
 
 	/* unreachable */
@@ -79,6 +96,7 @@ int main(int argc, char **argv)
 	sock_t master_socket;
 	int res;
 	struct sockaddr_in addr;
+	const int yes = 1;
 
 	res = daemon(0, 0);
 	if (res) {
@@ -91,6 +109,12 @@ int main(int argc, char **argv)
 	master_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (master_socket < 0) {
 		log_err("Failed to create master socket\n");
+		exit(-1);
+	}
+
+	res = setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+	if (res) {
+		log_err("Failed to setsockopt for master\n");
 		exit(-1);
 	}
 
