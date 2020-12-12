bf0fbe708fade30f08b38f957a1494c4c8512e9f
pskrgag <paskripkin@gmail.com>
2020-12-12 19:57:21 +0300

    makefile divided into compiling and linking stages

---
+++ build-console-Desktop-Debug/CMakeFiles/console.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pskrgag/Documents/bbb_workspace/bbb_os/console/console.cpp" "/home/pskrgag/Documents/bbb_workspace/bbb_os/build-console-Desktop-Debug/CMakeFiles/console.dir/console.cpp.o"
+  "/home/pskrgag/Documents/bbb_workspace/bbb_os/build-console-Desktop-Debug/console_autogen/mocs_compilation.cpp" "/home/pskrgag/Documents/bbb_workspace/bbb_os/build-console-Desktop-Debug/CMakeFiles/console.dir/console_autogen/mocs_compilation.cpp.o"
+  "/home/pskrgag/Documents/bbb_workspace/bbb_os/console/mainwindow.cpp" "/home/pskrgag/Documents/bbb_workspace/bbb_os/build-console-Desktop-Debug/CMakeFiles/console.dir/mainwindow.cpp.o"
+  "/home/pskrgag/Documents/bbb_workspace/bbb_os/console/olafclient.cpp" "/home/pskrgag/Documents/bbb_workspace/bbb_os/build-console-Desktop-Debug/CMakeFiles/console.dir/olafclient.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "console_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
